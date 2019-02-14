a34591d07b4d847485f51cf7a26e39f1ee5fd45c
Matthew Dargan <matthewdargan57@gmail.com>
2019-02-14 12:11:04 -0600

    Create regular socket message function

--- cozmo_network_controller.py
+++ cozmo_network_controller.py
@@ -1,7 +1,9 @@
 import asyncio
 from websocket_interface import WebsocketInterface
+import socket
+import time
 
-async def send_instructions():
+async def send_instructions_ws():
     s = WebsocketInterface(url='ws://10.0.1.10:5000')
     s.init()
 
@@ -10,3 +12,16 @@ async def send_instructions():
     s.ws.send('Cozmo117AE;F;L;300;200')
     await asyncio.sleep(0.5)
     s.ws.send('Cozmo117AE;20.8;45.2')
+
+def send_instructions_socket():
+    s = socket.socket()
+    s.connect(("10.0.1.10", 5000))
+
+    s.send(b'Cozmo117AE;56.4;20.3')
+    time.sleep(0.5)
+    s.send(b'Cozmo117AE;F;L;300;200')
+    time.sleep(0.5)
+    s.send(b'Cozmo117AE;20.8;45.2')
+
+if __name__ == "__main__":
+    send_instructions_socket()
